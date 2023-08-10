80974eee17afdab8a0c470d4adcddd2ce51d6c42
Mrunmayi <mrunmayinazirkar23@gmail.com>
2023-08-10 23:08:28 +0530

    Updates

---
+++ precache-manifest.2305c5b9cb66611e5eb1bec277b30fec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cb72b5e2d8611a3e5ff",
+    "url": "/textutils-website/static/css/main.85685dde.chunk.css"
+  },
+  {
+    "revision": "1cb72b5e2d8611a3e5ff",
+    "url": "/textutils-website/static/js/main.1cb72b5e.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils-website/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils-website/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "febcc86868c3e5dec025",
+    "url": "/textutils-website/static/js/runtime~main.febcc868.js"
+  },
+  {
+    "revision": "870176cc4930d276242410bc6ac1cd3b",
+    "url": "/textutils-website/index.html"
+  }
+];
